757c9d46042b5648658bcd70a95bb1589bbfe3d9
miiasandelin <n3sami00@students.oamk.fi>
2025-03-15 00:48:30 +0200

    Components & screens added

--- LocationApp/App.js
+++ LocationApp/App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 export default function App() {
   return (
